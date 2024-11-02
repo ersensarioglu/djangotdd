161ab7553276410d1bf62c35cef7a14eb7ef5a43
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-11-02 17:17:31 +0000

    test and develop model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+"""
+    class Post
+        -id:int
+        -title:varchar
+        -body:text
+        -created_at:datetime
+        -modified_at:datime
+"""
+
+class Post(models.Model):    # noqa: F811
+    id = models.IntegerField(primary_key=True)    # noqa: F811
+    title = models.CharField(max_length=50)    # noqa: F811
+    body = models.TextField()    # noqa: F811
+    created_at = models.DateTimeField(auto_now_add=True)    # noqa: F811
+    modified_at = models.DateTimeField(auto_now=True)    # noqa: F811
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class TestPostModel(TestCase):    # noqa: F811
+    def test_post_model_exists(self):    # noqa: F811
+        posts = Post.objects.count()
+        
+        self.assertEqual(posts, 0)
